401263779b7471f94048ebb832ad264dff6cdb84
WiniceS <wiphoton@163.com>
2020-08-16 22:06:41 +0800

    modify：添加mysql连接

--- back-end/Startup.cs
+++ back-end/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using back_end.Swagger;
+using Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace back_end
 {
@@ -30,6 +33,14 @@ namespace back_end
             services.AddSwaggerSetup();
             
             services.AddControllers();
+            
+            //数据库设置
+            services.AddDbContextPool<AppDbContext>(options => options
+                // replace with your connection string
+                .UseMySql(Configuration.GetConnectionString("ConnectionStrings"), mySqlOptions => mySqlOptions
+                    // replace with your Server Version and Type
+                    .ServerVersion(new Version(8, 0, 18), ServerType.MySql)
+                ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
